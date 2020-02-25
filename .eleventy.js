c8b466f1c84aaf48d376acf0c8ddf7b8451c56c6
Olivier Parent <olivpa@arteveldehs.be>
2020-02-25 13:58:48 +0100

    [WIP]

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    'css': 'assets/css',
+    'images': 'assets/images'
+  });
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addWatchTarget('./css/');
+
+  return {
+    dir: {
+      input: 'views',
+      output: 'docs'
+    },
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    pathPrefix: '/crowdfunding/' // Naam van de GitHub-repo
+  };
+};
